df63af9911f5ba84a305a23cf1339413eed616ff
Reinalyn1 <reinalyndiaded543@gmail.com>
2025-02-06 16:45:17 +0800

    ScrollView

---
+++ Activity1/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {Image, ScrollView, Text} from 'react-native';
+
+const App = () => (
+  <ScrollView>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+    <text>scroll me </text>
+   <text>scroll me </text>
+   <text>scroll me </text>
+   <text>scroll me </text>
+   <text>scroll me </text>
+   <text>scroll me </text>
+
+  </ScrollView>
+);
+
+export default App;
